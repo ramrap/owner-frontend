5f73f275eafe78b2eed67dced2b5600c5b288403
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2021-09-21 19:15:19 +0530

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -8,7 +8,7 @@ const baseURL = {
 };
 
 const axiosInstance = axios.create({
-    baseURL: baseURL.local,
+    baseURL: baseURL.testing,
     timeout: 25000,
     headers: {
         Authorization: cookie.load("access_token") ? `Bearer ${cookie.load("access_token")}` : "",
