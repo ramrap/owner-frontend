24626cabe9d614b91a2be5ec9739a2cbad5c146e
RAHUL PARMAR <cs1190387@iitd.ac.in>
2021-10-15 00:07:13 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     webpack5: true,
     eslint: {
         // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
+        // your project has ESLint error.
         ignoreDuringBuilds: true,
       },
 };
